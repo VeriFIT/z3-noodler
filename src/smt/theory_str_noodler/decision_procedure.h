04349ae8d9956470ec497851ac8bd0bff04f7b45
vhavlena <vohavlena@gmail.com>
2024-06-27 19:19:03 +0200

    disequations: heur; cleaning

--- src/smt/theory_str_noodler/decision_procedure.h
+++ src/smt/theory_str_noodler/decision_procedure.h
@@ -384,21 +384,6 @@ namespace smt::noodler {
          */
         Formula not_contains{};
 
-        // /**
-        //  * @brief Construct constraints to get rid of not_contains predicates.
-        //  * @return l_false -> unsatisfiable constaint; l_undef if it is not evident
-        //  */
-        // lbool replace_not_contains();
-
-        // /**
-        //  * @brief Check if it is possible to syntactically unify not contains terms. If they are included (in the sense of vectors) the 
-        //  * not(contain) is unsatisfiable.
-        //  * 
-        //  * @param prep FormulaPreprocessor
-        //  * @return l_true -> can be unified 
-        //  */
-        // lbool can_unify_not_contains(const FormulaPreprocessor& prep);
-
     public:
 
         /**
@@ -425,8 +410,6 @@ namespace smt::noodler {
             conversions(conversions),
             m_params(par) {
             
-            // we extract from the input formula all not_contains predicates and add them to not_contains formula
-            // this->formula.extract_predicates(PredicateType::NotContains, this->not_contains);
         }
         
         /**
