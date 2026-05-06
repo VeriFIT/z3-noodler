f6a901647352014a656640e10792c8b5ccd35de8
Ondřej Koumar <xkouma02@vutbr.cz>
2026-05-06 17:39:35 +0200

    refactor: Replace debug standard output with STRACE for ECMA constraints generation

--- src/smt/theory_str_noodler/theory_str_noodler.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler.cpp
@@ -2005,18 +2005,11 @@ namespace smt::noodler {
         SASSERT(is_app(x));
         const expr_ref ecma_formula = builder.generate_constraints(to_app(x));
 
-#ifndef NDEBUG
-        std::cout << ";; --- GENERATED ECMA CONSTRAINTS ---" << std::endl;
-        std::cout << mk_pp(ecma_formula.get(), m) << std::endl;
-        std::cout << ";; ----------------------------------" << std::endl;
-#endif
-        // 5. AXIOMATIZACE: e <=> ecma_formula
-        const expr_ref lemma(m.mk_iff(e, ecma_formula), m);
+        STRACE(str, tout << ";; --- GENERATED ECMA CONSTRAINTS ---" << std::endl;
+              tout << mk_pp(ecma_formula.get(), m) << std::endl;
+              tout << ";; ----------------------------------" << std::endl;);
 
-        // 6. Propagace lemmatu do Z3 jádra
-        // V Z3 theory pluginech se nová pravidla/axiomy přidávají pomocí kontextu.
-        // Typicky se to dělá internalizací lemmatu bez vyžadování důkazu.
-        add_axiom(lemma);
+        add_axiom(expr_ref(m.mk_iff(e, ecma_formula), m));
     }
 
     void theory_str_noodler::handle_in_re(expr* const e, const bool is_true) {
