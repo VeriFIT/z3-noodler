6a73f3e301a6bf81d4c9fa86bd90dd523c6017ca
jurajsic <juraj.sic@outlook.com>
2026-09-03 13:16:50 +0200

    disable warnings for MATCH_* functions in seq_decl_plugin

--- src/ast/seq_decl_plugin.h
+++ src/ast/seq_decl_plugin.h
@@ -720,12 +720,12 @@ public:
         MATCH_UNARY(is_of_pred);
         MATCH_UNARY(is_reverse);
         MATCH_BINARY(is_derivative);
+        MATCH_UNARY(is_from_ecma2020);
         END_DISABLE_WARNING;
         bool is_loop(expr const* n, expr*& body, unsigned& lo, unsigned& hi) const;
         bool is_loop(expr const* n, expr*& body, unsigned& lo) const;
         bool is_loop(expr const* n, expr*& body, expr*& lo, expr*& hi) const;
         bool is_loop(expr const* n, expr*& body, expr*& lo) const;
-        MATCH_UNARY(is_from_ecma2020);
         bool is_from_ecma2020(expr const* n, zstring& pattern) const { expr *s; return is_from_ecma2020(n, s) && u.str.is_string(s, pattern); } 
         unsigned min_length(expr* r) const;
         unsigned max_length(expr* r) const;
@@ -816,6 +816,7 @@ public:
         bool is_left_extend(expr const* n)    const { return is_app_of(n, m_fid, OP_RAT_LEFT_EXTEND); }
         bool is_right_extend(expr const* n)    const { return is_app_of(n, m_fid, OP_RAT_RIGHT_EXTEND); }
 
+        START_DISABLE_EXTRA_SEMI_WARNING;
         MATCH_BINARY(is_to_rat);
         MATCH_BINARY(is_concat);
         MATCH_BINARY(is_union);
@@ -827,6 +828,7 @@ public:
         MATCH_UNARY(is_identity);
         MATCH_UNARY(is_left_extend);
         MATCH_UNARY(is_right_extend);
+        END_DISABLE_WARNING;
 
         bool is_epsilon(expr const* r) { expr *s, *t; return is_to_rat(r, s, t) && u.str.is_empty(s) && u.str.is_empty(t); }
         app* mk_epsilon() { return mk_to_rat(u.str.mk_empty(u.mk_string_sort()), u.str.mk_empty(u.mk_string_sort())); };
