e6b30bcd6077b68dfd4fea7b90c9555d243abb70
jurajsic <juraj.sic@outlook.com>
2026-09-14 16:07:48 +0200

    move nneded vars from conversions loop from decision procedure to check_len_sat

--- src/smt/theory_str_noodler/theory_str_noodler_final_check.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler_final_check.cpp
@@ -930,6 +930,10 @@ namespace smt::noodler {
             }
         }
 
+        for (const TermConversion& conv : m_conversion_todo) {
+            needed_vars.insert(conv.number_var);
+        }
+
         expr_ref_vector conjuncts(m);
         flatten_and(model_formula.get(), conjuncts);
         expr_ref_vector kept(m);
